c36204a3806f04a546d147fde2797df4336094d8
Felipe <113586743+felipebuleche@users.noreply.github.com>
2022-12-13 18:30:45 +0100

    Add files via upload

---
+++ Php/Php(Formulary)/Muestradatos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muestradatos.php</title>
+</head>
+<body>
+<?php
+    $Name=$_GET["name_usuario"];
+    $Surname=$_GET["surname_usuario"];
+    echo "Hola $Name $Surname";
+?> 
+
+</body>
+</html>
